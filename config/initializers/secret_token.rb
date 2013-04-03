a5c94757ee07e4e8bd7ab058df3622f52aec177a
Eric and Peter Lin <student@appacademy.io>
2013-04-03 12:59:57 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '0751f0ca7ac411681bc3329af0c20ae16c4f15026a456cd7646a4570520d1c39c9ffc063bd7ad4480b61592181b1b39f6e780157d6a7f68f76cc99245504e485'
